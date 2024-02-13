cbbd9afc88fc1822ce0ee612bf08cb4e5fc29e17
v-lane <victoria.lane.m@gmail.com>
2024-02-13 12:02:57 -0500

    created typewriter function

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+let timer = 0;
+
+for (const char of sentence) {
+  setTimeout(() => process.stdout.write(char), timer);
+  timer += 50;
+}
+
+setTimeout(() => console.log(), timer);
